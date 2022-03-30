54b7cd10d3be5acda4c3fce447c04f64a6e0b016
Seokyong Shin <kistssy@gmail.com>
2022-03-30 15:43:17 +0900

    train: Reformat code

--- train.py
+++ train.py
@@ -17,7 +17,6 @@ import time
 import datetime
 import argparse
 
-
 respth = './res'
 if not osp.exists(respth):
     os.makedirs(respth)
@@ -27,11 +26,11 @@ logger = logging.getLogger()
 def parse_args():
     parse = argparse.ArgumentParser()
     parse.add_argument(
-            '--local_rank',
-            dest = 'local_rank',
-            type = int,
-            default = -1,
-            )
+        '--local_rank',
+        dest='local_rank',
+        type=int,
+        default=-1,
+    )
     return parse.parse_args()
 
 
@@ -39,11 +38,11 @@ def train():
     args = parse_args()
     torch.cuda.set_device(args.local_rank)
     dist.init_process_group(
-                backend = 'nccl',
-                init_method = 'tcp://127.0.0.1:33241',
-                world_size = torch.cuda.device_count(),
-                rank=args.local_rank
-                )
+        backend='nccl',
+        init_method='tcp://127.0.0.1:33241',
+        world_size=torch.cuda.device_count(),
+        rank=args.local_rank
+    )
     setup_logger(respth)
 
     # dataset
@@ -56,12 +55,12 @@ def train():
     ds = FaceMask(data_root, cropsize=cropsize, mode='train')
     sampler = torch.utils.data.distributed.DistributedSampler(ds)
     dl = DataLoader(ds,
-                    batch_size = n_img_per_gpu,
-                    shuffle = False,
-                    sampler = sampler,
-                    num_workers = n_workers,
-                    pin_memory = True,
-                    drop_last = True)
+                    batch_size=n_img_per_gpu,
+                    shuffle=False,
+                    sampler=sampler,
+                    num_workers=n_workers,
+                    pin_memory=True,
+                    drop_last=True)
 
     # model
     ignore_idx = -100
@@ -69,16 +68,16 @@ def train():
     net.cuda()
     net.train()
     net = nn.parallel.DistributedDataParallel(net,
-            device_ids = [args.local_rank, ],
-            output_device = args.local_rank
-            )
+                                              device_ids=[args.local_rank, ],
+                                              output_device=args.local_rank
+                                              )
     score_thres = 0.7
-    n_min = n_img_per_gpu * cropsize[0] * cropsize[1]//16
+    n_min = n_img_per_gpu * cropsize[0] * cropsize[1] // 16
     LossP = OhemCELoss(thresh=score_thres, n_min=n_min, ignore_lb=ignore_idx)
     Loss2 = OhemCELoss(thresh=score_thres, n_min=n_min, ignore_lb=ignore_idx)
     Loss3 = OhemCELoss(thresh=score_thres, n_min=n_min, ignore_lb=ignore_idx)
 
-    ## optimizer
+    # optimizer
     momentum = 0.9
     weight_decay = 5e-4
     lr_start = 1e-2
@@ -87,16 +86,16 @@ def train():
     warmup_steps = 1000
     warmup_start_lr = 1e-5
     optim = Optimizer(
-            model = net.module,
-            lr0 = lr_start,
-            momentum = momentum,
-            wd = weight_decay,
-            warmup_steps = warmup_steps,
-            warmup_start_lr = warmup_start_lr,
-            max_iter = max_iter,
-            power = power)
+        model=net.module,
+        lr0=lr_start,
+        momentum=momentum,
+        wd=weight_decay,
+        warmup_steps=warmup_steps,
+        warmup_start_lr=warmup_start_lr,
+        max_iter=max_iter,
+        power=power)
 
-    ## train loop
+    # train loop
     msg_iter = 50
     loss_avg = []
     st = glob_st = time.time()
@@ -129,7 +128,7 @@ def train():
         loss_avg.append(loss.item())
 
         #  print training log message
-        if (it+1) % msg_iter == 0:
+        if (it + 1) % msg_iter == 0:
             loss_avg = sum(loss_avg) / len(loss_avg)
             lr = optim.lr
             ed = time.time()
@@ -137,24 +136,24 @@ def train():
             eta = int((max_iter - it) * (glob_t_intv / it))
             eta = str(datetime.timedelta(seconds=eta))
             msg = ', '.join([
-                    'it: {it}/{max_it}',
-                    'lr: {lr:4f}',
-                    'loss: {loss:.4f}',
-                    'eta: {eta}',
-                    'time: {time:.4f}',
-                ]).format(
-                    it = it+1,
-                    max_it = max_iter,
-                    lr = lr,
-                    loss = loss_avg,
-                    time = t_intv,
-                    eta = eta
-                )
+                'it: {it}/{max_it}',
+                'lr: {lr:4f}',
+                'loss: {loss:.4f}',
+                'eta: {eta}',
+                'time: {time:.4f}',
+            ]).format(
+                it=it + 1,
+                max_it=max_iter,
+                lr=lr,
+                loss=loss_avg,
+                time=t_intv,
+                eta=eta
+            )
             logger.info(msg)
             loss_avg = []
             st = ed
         if dist.get_rank() == 0:
-            if (it+1) % 5000 == 0:
+            if (it + 1) % 5000 == 0:
                 state = net.module.state_dict() if hasattr(net, 'module') else net.state_dict()
                 if dist.get_rank() == 0:
                     torch.save(state, './res/cp/{}_iter.pth'.format(it))
