eac54b916b2d1ac2df1f4296eefeeab2bdd69fa0
Seokyong Shin <kistssy@gmail.com>
2024-01-05 09:42:10 +0900

    Update prepropess_data.py

--- prepropess_data.py
+++ prepropess_data.py
@@ -1,40 +1,45 @@
-import os.path as osp
+import argparse
 import os
+
 import cv2
-from transform import *
+import numpy as np
+import tqdm
 from PIL import Image
 
-# 데이터셋 폴더 위치를 지정하세요.
-dataset_dir = 'D:\\data\\CelebAMask-HQ'
 
-face_data = os.path.join(dataset_dir, 'CelebAMask-HQ-img')
-face_sep_mask = os.path.join(dataset_dir, 'CelebAMask-HQ-mask-anno')
-mask_path = os.path.join(dataset_dir, 'mask')
-os.makedirs(mask_path, exist_ok=True)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_dir', type=str, required=True)
+    args = parser.parse_args()
+    return args
 
-counter = 0
-total = 0
-for i in range(15):
 
-    atts = ['skin', 'l_brow', 'r_brow', 'l_eye', 'r_eye', 'eye_g', 'l_ear', 'r_ear', 'ear_r',
-            'nose', 'mouth', 'u_lip', 'l_lip', 'neck', 'neck_l', 'cloth', 'hair', 'hat']
+def main(args):
+    face_data = os.path.join(args.dataset_dir, 'CelebAMask-HQ-img')
+    face_sep_mask = os.path.join(args.dataset_dir, 'CelebAMask-HQ-mask-anno')
+    output_mask_path = os.path.join(args.dataset_dir, 'mask')
+    os.makedirs(output_mask_path, exist_ok=True)
 
-    for j in range(i * 2000, (i + 1) * 2000):
+    attributes = ['skin', 'l_brow', 'r_brow', 'l_eye', 'r_eye', 'eye_g', 'l_ear', 'r_ear', 'ear_r',
+                  'nose', 'mouth', 'u_lip', 'l_lip', 'neck', 'neck_l', 'cloth', 'hair', 'hat']
+    counter = 0
+    total = 0
+    for i in tqdm.tqdm(range(15), desc='Preprocess'):
+        for j in tqdm.tqdm(range(i * 2000, (i + 1) * 2000), 'Subset'):
+            mask = np.zeros((512, 512))
+            for index, attr in enumerate(attributes, 1):
+                total += 1
+                file_name = ''.join([str(j).rjust(5, '0'), '_', attr, '.png'])
+                path = os.path.join(face_sep_mask, str(i), file_name)
+                if os.path.exists(path):
+                    counter += 1
+                    sep_mask = np.array(Image.open(path).convert('P'))
 
-        mask = np.zeros((512, 512))
+                    mask[sep_mask == 225] = index
+            cv2.imwrite(os.path.join(output_mask_path, f'{j}.png'), mask)
+    print(f'{counter=}, {total=}')
 
-        for l, att in enumerate(atts, 1):
-            total += 1
-            file_name = ''.join([str(j).rjust(5, '0'), '_', att, '.png'])
-            path = osp.join(face_sep_mask, str(i), file_name)
 
-            if os.path.exists(path):
-                counter += 1
-                sep_mask = np.array(Image.open(path).convert('P'))
-                # print(np.unique(sep_mask))
-
-                mask[sep_mask == 225] = l
-        cv2.imwrite('{}/{}.png'.format(mask_path, j), mask)
-        print(j)
-
-print(counter, total)
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
